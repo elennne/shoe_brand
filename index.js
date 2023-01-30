b60baa42a83f09d5bbf17314409da66297ddce70
ელენე ჩანადირი <117665219+elennne@users.noreply.github.com>
2023-01-30 21:12:54 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let answer = "ecco";
+let question = prompt("what is my favorite shoe brand?");
+let result = question.toLowerCase();
+if (answer==result) {
+    document.write("yes, Ecco is my favorite shoe brand");
+}
+else {
+    document.write ("No, your answer is wrong. My favorite shoe brand is ecco.")
+}
